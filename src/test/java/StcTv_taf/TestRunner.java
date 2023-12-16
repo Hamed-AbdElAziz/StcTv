9508e12df5b268906c9d3508ebb3b941e00b8bb5
Hamed-AbdElAziz <abdelaziz.hamed.m1992@gmail.com>
2023-12-16 22:15:18 +0200

    add TestRunner class

---
+++ src/test/java/StcTv_taf/TestRunner.java
@@ -0,0 +1,12 @@
+package StcTv_taf;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/java/StcTv_taf/tests"},
+        glue = {"StcTv_taf.tests"},
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
